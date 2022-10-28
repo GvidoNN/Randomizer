810dcc1e5f0534980030bdde46e0fbdc0df6a2d2
GvidoNN <khodosok.2004@gmail.com>
2022-10-28 19:37:52 +0300

    make Bottom Navigation View and make navigation(not ready at all)

--- app/src/main/java/my/lovely/randomizer2/screens/FragmentChee.kt
+++
@@ -1,8 +0,0 @@
-package my.lovely.randomizer2.screens
-
-import androidx.fragment.app.Fragment
-import my.lovely.randomizer2.R
-
-class FragmentChee : Fragment(R.layout.fragment_chee) {
-
-}
--- app/src/main/java/my/lovely/randomizer2/screens/FragmentMain.kt
+++ app/src/main/java/my/lovely/randomizer2/screens/FragmentMain.kt
@@ -5,8 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import my.lovely.randomizer2.R
 
 class FragmentMain : Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.mainBottomNavigationView)
+        val navController = (childFragmentManager.findFragmentById(R.id.mainContainerView) as NavHostFragment).navController
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
 
 }
