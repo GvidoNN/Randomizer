5672abc83927653efbe0ddb703facbf55537fc0d
GvidoNN <khodosok.2004@gmail.com>
2022-10-28 18:22:40 +0300

    Create New project and create a few fragments

---
+++ app/src/main/java/my/lovely/randomizer2/screens/FragmentChee.kt
@@ -0,0 +1,8 @@
+package my.lovely.randomizer2.screens
+
+import androidx.fragment.app.Fragment
+import my.lovely.randomizer2.R
+
+class FragmentChee : Fragment(R.layout.fragment_chee) {
+
+}
---
+++ app/src/main/java/my/lovely/randomizer2/screens/FragmentTeams.kt
@@ -0,0 +1,12 @@
+package my.lovely.randomizer2.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.lovely.randomizer2.R
+
+class FragmentTeams : Fragment(R.layout.fragment_teams) {
+
+}
