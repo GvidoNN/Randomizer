3764cdb16ace7cebebfeae386e792976f0361d69
GvidoNN <khodosok.2004@gmail.com>
2022-11-09 19:26:37 +0300

    make dice fragment

--- app/src/main/java/my/lovely/randomizer2/screens/FragmentDice.kt
+++ app/src/main/java/my/lovely/randomizer2/screens/FragmentDice.kt
@@ -1,12 +1,73 @@
 package my.lovely.randomizer2.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import my.lovely.randomizer2.R
 
 class FragmentDice : Fragment(R.layout.fragment_dice) {
 
+    lateinit var imDice1: ImageView
+    lateinit var imDice2: ImageView
+    lateinit var imDice3: ImageView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onDiceTap()
+    }
+
+    private fun findView(){
+        imDice1 = requireView().findViewById(R.id.imDice1)
+        imDice2 = requireView().findViewById(R.id.imDice2)
+        imDice3 = requireView().findViewById(R.id.imDice3)
+    }
+
+    private fun onDiceTap() {
+        findView()
+        imDice1.setOnClickListener {
+            rollDice(randomNumber(), imDice1)
+        }
+        imDice2.setOnClickListener {
+            rollDice(randomNumber(), imDice2)
+        }
+        imDice3.setOnClickListener {
+            rollDice(randomNumber(), imDice3)
+        }
+    }
+
+    private fun rollDice(imageId: Int, imDice: ImageView) {
+        imDice.animate().apply {
+            duration = 1000
+            rotationYBy(1800f)
+            rotationXBy(1800f)
+            imDice.isClickable = false
+        }.withEndAction {
+            imDice.setImageResource(imageId)
+            imDice.isClickable = true
+        }.start()
+    }
+
+    private fun randomNumber(): Int {
+        val number = (1..6).random()
+        return when (number) {
+            1 -> R.drawable.ic_dice1
+            2 -> R.drawable.ic_dice2
+            3 -> R.drawable.ic_dice3
+            4 -> R.drawable.ic_dice4
+            5 -> R.drawable.ic_dice5
+            6 -> R.drawable.ic_dice6
+            else -> R.drawable.ic_cube
+        }
+    }
 }
